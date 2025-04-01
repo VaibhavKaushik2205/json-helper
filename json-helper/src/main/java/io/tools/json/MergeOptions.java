50c962fbd0aa80018ad0edcfdcf56638809f2101
Vaibhav Kaushik <vaibhavkaushik.2205@gmail.com>
2025-04-01 15:37:06 +0530

    Added boolean option to handle empty string update

--- json-helper/src/main/java/io/tools/json/MergeOptions.java
+++ json-helper/src/main/java/io/tools/json/MergeOptions.java
@@ -11,6 +11,7 @@ public class MergeOptions {
 
     MergeStrategy arrayMergeStrategy = MergeStrategy.APPEND;
     MergeStrategy objectMergeStrategy = MergeStrategy.OVERWRITE;
+    boolean IGNORE_EMPTY_STRINGS = true;
 
     BiPredicate<ArrayNode, JsonNode> customMergeStrategy = null;
 
@@ -24,8 +25,25 @@ public class MergeOptions {
         return this;
     }
 
+    public MergeOptions setIgnoreEmptyStrings(boolean ignoreEmptyStrings) {
+        this.IGNORE_EMPTY_STRINGS = ignoreEmptyStrings;
+        return this;
+    }
+
     public MergeOptions setCustomMergeStrategy(BiPredicate<ArrayNode, JsonNode> strategy) {
         this.customMergeStrategy = strategy;
         return this;
+    }
+
+    public MergeStrategy getArrayMergeStrategy() {
+        return this.arrayMergeStrategy;
+    }
+
+    public MergeStrategy getObjectMergeStrategy() {
+        return this.objectMergeStrategy;
+    }
+
+    public boolean getIgnoreEmptyStrings() {
+        return this.IGNORE_EMPTY_STRINGS;
     }
 }
