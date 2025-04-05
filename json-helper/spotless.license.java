81036a11696932fd21e97e7334c406cd0d9db12c
Vaibhav Kaushik <vaibhavkaushik.2205@gmail.com>
2025-04-05 17:05:42 +0530

    updated codeowners and spotless check

--- json-helper/spotless.license.java
+++ json-helper/spotless.license.java
@@ -2,6 +2,4 @@
  * Copyright (c) 2024 Vaibhav Kaushik
  * All rights reserved.
  *
- * This source code is licensed under the MIT-style license found in the
- * LICENSE file in the root directory of this source tree.
  */
--- json-helper/src/main/java/io/tools/json/enums/MergeStrategy.java
+++ json-helper/src/main/java/io/tools/json/enums/MergeStrategy.java
@@ -2,8 +2,6 @@
  * Copyright (c) 2024 Vaibhav Kaushik
  * All rights reserved.
  *
- * This source code is licensed under the MIT-style license found in the
- * LICENSE file in the root directory of this source tree.
  */
 package io.tools.json.enums;
 
