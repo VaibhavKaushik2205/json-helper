0102aff248ec565d8cdd8d7b0d6506fe8295e6b9
Vaibhav Kaushik <vaibhavkaushik.2205@gmail.com>
2025-04-05 16:14:28 +0530

    Added spotless plugin for code formatting

--- json-helper/spotless.license.java
+++ json-helper/spotless.license.java
@@ -5,11 +5,3 @@
  * This source code is licensed under the MIT-style license found in the
  * LICENSE file in the root directory of this source tree.
  */
-package io.tools.json.enums;
-
-public enum MergeStrategy {
-  OVERWRITE,
-  APPEND,
-  UNIQUE,
-  DEEP_MERGE
-}
